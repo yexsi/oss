66e9d37c8956dd16562c96f3b6e624bd41ba9560
yexsi <63133923+yexsi@users.noreply.github.com>
2020-08-26 18:53:28 +0800

    add

--- 复制版权.js
+++ 复制版权.js
@@ -1,8 +1,18 @@
-<script>
-        let oDiv = document.querySelector('div');
-        oDiv.oncopy = function(e) { // 复制事件
-            e.preventDefault();
-            let copyMsg = window .getSelection() + '商业转载请注明出处。'; // window .getSelection() 表示选择的内容
-            e.clipboardData.setData("Text", copyMsg); // 将复制信息添加到剪切板
-        }
+<script language="javascript" type="text/javascript">
+jQuery(document).on('copy', function(e)
+	{
+	  var selected = window.getSelection();
+	  var selectedText = selected.toString().replace(/\n/g, '<br>');  // Solve the line breaks conversion issue
+	  var copyFooter = '<br>---------------------<br>著作权归作者所有。<br>' 
+	                        + '商业转载请联系作者获得授权，非商业转载请注明出处。<br>'
+	                        + '作者：Bravo Yeung<br> 源地址：' + document.location.href
+	                        + '<br>来源：博客园cnblogs<br>© 版权声明：本文为博主原创文章，转载请附上博文链接！';
+	  var copyHolder = $('<div>', {id: 'temp', html: selectedText + copyFooter, style: {position: 'absolute', left: '-99999px'}});
+	    
+	  $('body').append(copyHolder);
+	  selected.selectAllChildren( copyHolder[0] );
+	  window.setTimeout(function() {
+	      copyHolder.remove();
+	  },0);
+	});
 </script>
